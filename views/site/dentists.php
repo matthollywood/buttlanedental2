bbda48429e5442ed3cbae25c05a073a7b5e4397d
matthollywood <matthew_jones81@yahoo.co.uk>
2018-02-02 20:25:04 +0000

    Altered and updated dentist page, added dental nurses, added images

--- views/site/dentists.php
+++ views/site/dentists.php
@@ -7,42 +7,44 @@ use yii\helpers\Html;
 $this->title = 'Dentists';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class="site-dentists">
 
     <div class ="body-content">
     <div class="row">
-  <div class="col-lg-4 col-lg-offset-2" >
+  <div class="col-lg-11 col-lg-offset-1" >
     <div class="card" style="height: 100%">
-      <img src="images/john.jpg" class="img-center" alt="John" style="width:75%;">
+      <img src="images/john.jpg" class="img-center" alt="John" style="float: left; width:25%; padding: 0px 15px 15px 0px;">
       <div class="dent-title">
         <h2>Dr John Blayney</h2>
       </div>
         <p class="title">Dentist</p>
         <p>Dr Blayney BDS qualified as a dentist from Manchester University and has been registered as a general dental practitioner since 1984 (GDC no. 59451).  </p>
         <p>He particularly enjoys looking after children and nervous patients and has a special interest in cosmetic treatments.</p>
-      
+
     </div>
   </div>
-    <div class="col-lg-4">
-    <div class="card">
-      <img src="images/jane.jpg" alt="jane" style="width:75%">
+  </div>
+  <div class="row">
+      <div class="col-lg-11 col-lg-offset-1" >
+    <div class="card" style ="height: 100%">
+      <img src="images/jane.jpg" alt="jane" style="float: left; width:25%; padding: 0px 15px 15px 0px;">
       <div class="dent-title">
         <h2>Dr Jane Dougherty</h2>
       </div>
         <p class="title">Dentist</p>
         <p>
-Dr Dougherty BDS qualified from Manchester University in 1989 and has since been working as a general dental practitioner (GDC no. 64846).  
+Dr Dougherty BDS qualified from Manchester University in 1989 and has since been working as a general dental practitioner (GDC no. 64846).
 She has previously worked in practices in Leicester and Macclesfield before joining Butt Lane Dental Surgery in 2009.</p>
         <p>She enjoys working  with our friendly staff and patients from a wide range of backgrounds</p>
-      
+
     </div>
   </div>
-          
+
     </div>
         </div>
 
-    
- 
+
+
 
 
 
